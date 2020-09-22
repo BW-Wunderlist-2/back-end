9cf11050f9c2deec539426373ab01b50e47bf230
samcode206 <sameem-hadi@lambdastudents.com>
2020-09-21 19:01:07 -0700

    final

--- knexfile.js
+++ knexfile.js
@@ -5,14 +5,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/db.db3'
+      filename: './data/db.sqlite3'
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    }
-    ,
+    
     migrations: {
       directory: "./data/migrations"
     },
